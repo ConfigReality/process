c8168feacb1b607f8ab72e558b1d53ba8bae7002
SalvatoreLa <salvatore.la.spata@gmail.com>
2023-11-10 14:19:39 +0100

    log

--- src/utils.js
+++ src/utils.js
@@ -24,12 +24,12 @@ const walk = async (currentDirPath, callback) => {
     const filepath = path.join(currentDirPath, file)
     const _stat = await stat(filepath)
     const _path = path.extname(file);
-
+    console.log(_path)
     if (_stat.isFile())
       ret.push({
         file: readFile(filepath),
         filename: file,
-        contentType: mime.extension(_path),
+        contentType: mime.lookup(_path),
         path: filepath.substring(currentDirPath.length + 1),
       })
     else if (_stat.isDirectory()) ret.push(...(await walk(filepath, callback)))
